6ba8e3c2377d700c4fe499baa47d1522ab813040
PSD <psdubowik@gmail.com>
2017-10-29 06:43:50 +0100

    Cleaning html and js code

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
 
     var span = $("span");
     span.each(function(index, element){
-        if(index%2 == 0) {
+        if (index % 2 == 0) {
             $(element).css("color", "red");
         };
     });
 
     var paragraphs = $('p');
     paragraphs.each(function(index, element){
-        var button = '<button class="btn btn-success btn-lg" data-tmp="' + "My index number is "+ index + "."+'">Click me</button>';
-        $(element).append('<br>' + button)
+        var button = '<button class="btn btn-success btn-lg" data-tmp="' + "My index number is "+ index + "." + '">Click me</button>';
+        $(element).append('<br>' + button);
     });
 
-    $("button").click(function(){
+    $("button").on("click", function(){
         alert($(this).attr("data-tmp"));
     });
 });
